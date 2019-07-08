99eafa0f646d6c495ca4b83c53d7d99f3e7b7bde
up1 <somkiat.p@gmail.com>
2019-07-08 12:11:25 +0700

    Hello REST

---
+++ src/main/java/com/example/demo/Hello2Controller.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello2Controller {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse sayHi(@PathVariable String name) {
+        return new HelloResponse("Hello, " + name);
+    }
+
+}
+
+class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
